bf48108ba0b275c393e1f7686c5100c7f3a93003
ghiscoding <gbeaulac@gmail.com>
2019-11-05 00:09:47 -0500

    Redo Aurelia Demo for WebPack 4 with Bootstrap 4 - redo the demo with latest version of the aurelia-cli and also because previous version didn't work anymore

--- webpack-bs4-demo/src/main.ts
+++ webpack-bs4-demo/src/main.ts
@@ -1,17 +1,17 @@
-/// <reference types="aurelia-loader-webpack/src/webpack-hot-interface"/>
+import { Aurelia } from 'aurelia-framework'
+import { PLATFORM } from 'aurelia-pal';
+import { TCustomAttribute } from 'aurelia-i18n';
+import * as Backend from 'i18next-xhr-backend';
 
-// we want font-awesome to load as soon as possible to show the fa-spinner
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 import 'flatpickr/dist/flatpickr.min.css';
-import 'aurelia-slickgrid/dist/lib/multiple-select/multiple-select.css';
-import 'aurelia-slickgrid/dist/lib/multiple-select/multiple-select.js';
 import './styles/styles.scss';
 import './styles/slickgrid-styles.scss';
-import { Aurelia, PLATFORM } from 'aurelia-framework';
-import environment from './environment';
-import { TCustomAttribute } from 'aurelia-i18n';
-import * as Backend from 'i18next-xhr-backend';
+import 'aurelia-slickgrid/dist/lib/multiple-select/multiple-select.css';
+import 'aurelia-slickgrid/dist/lib/multiple-select/multiple-select.js';
+// import environment from './environment';
+import * as environment from '../config/environment.json';
 import * as Bluebird from 'bluebird';
 import 'bootstrap';
 
@@ -23,6 +23,9 @@ Bluebird.config({
   longStackTraces: false
 });
 
+// if you use CSS instead of SASS
+import 'aurelia-slickgrid/dist/styles/css/slickgrid-theme-bootstrap.css';
+
 export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
@@ -33,31 +36,28 @@ export function configure(aurelia: Aurelia) {
     config.options.gridMenu.iconCssClass = 'fa fa-bars';
   });
 
-  aurelia.use.plugin(PLATFORM.moduleName('aurelia-i18n'), (instance) => {
+  aurelia.use.plugin(PLATFORM.moduleName('aurelia-i18n'), instance => {
     let aliases = ['t', 'i18n'];
-      // add aliases for 't' attribute
-      TCustomAttribute.configureAliases(aliases);
+    // add aliases for 't' attribute
+    TCustomAttribute.configureAliases(aliases);
 
-      // register backend plugin
-      instance.i18next.use(Backend);
+    // register backend plugin
+    instance.i18next.use(Backend);
 
-      // adapt options to your needs (see http://i18next.com/docs/options/)
-      // make sure to return the promise of the setup method, in order to guarantee proper loading
-      return instance.setup({
-        backend: {                                  // <-- configure backend settings
-          loadPath: './assets/locales/{{lng}}/{{ns}}.json', // <-- XHR settings for where to get the files from
-        },
-        attributes: aliases,
-        lng : 'en',
-        fallbackLng : 'en',
-        debug : false
-      });
+    // adapt options to your needs (see http://i18next.com/docs/options/)
+    // make sure to return the promise of the setup method, in order to guarantee proper loading
+    return instance.setup({
+      backend: {                                  // <-- configure backend settings
+        loadPath: 'assets/locales/{{lng}}/{{ns}}.json', // <-- XHR settings for where to get the files from
+      },
+      attributes: aliases,
+      lng: 'en',
+      fallbackLng: 'en',
+      debug: false
+    });
   });
 
-
-  if (environment.debug) {
-    aurelia.use.developmentLogging();
-  }
+  aurelia.use.developmentLogging(environment.debug ? 'debug' : 'warn');
 
   if (environment.testing) {
     aurelia.use.plugin(PLATFORM.moduleName('aurelia-testing'));
