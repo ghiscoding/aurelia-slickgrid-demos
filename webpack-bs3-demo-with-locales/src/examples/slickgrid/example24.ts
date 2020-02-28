2cb08fd73a1cafd67bca9ec59b31bfc02ccc64be
ghiscoding <gbeaulac@gmail.com>
2020-02-27 21:33:07 -0500

    update to latest version 2.17.6

--- webpack-bs3-demo-with-locales/src/examples/slickgrid/example24.ts
+++ webpack-bs3-demo-with-locales/src/examples/slickgrid/example24.ts
@@ -238,9 +238,12 @@ export class Example24 {
       enableFiltering: true,
       enableSorting: true,
       excelExportOptions: {
-        exportWithFormatter: true
-      },
+        exportWithFormatter: true,
+        customColumnWidth: 15,
 
+        // you can customize how the header titles will be styled (defaults to Bold)
+        columnHeaderStyle: { font: { bold: true, italic: true } }
+      },
       enableContextMenu: true,
       enableCellMenu: true,
 
