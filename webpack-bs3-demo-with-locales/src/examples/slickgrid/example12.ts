fafb94e9f3c8ac92a683ce395a725d5ad9098cc9
ghiscoding <gbeaulac@gmail.com>
2020-03-03 19:47:24 -0500

    update to latest version 2.17.8

--- webpack-bs3-demo-with-locales/src/examples/slickgrid/example12.ts
+++ webpack-bs3-demo-with-locales/src/examples/slickgrid/example12.ts
@@ -159,15 +159,15 @@ export class Example12 {
   dynamicallyAddTitleHeader() {
     // you can dynamically add your column to your column definitions
     // and then use the spread operator [...cols] OR slice to force Aurelia to review the changes
-    const newCol = { id: `title${this.duplicateTitleHeaderCount++}`, field: 'id', nameKey: 'TITLE', formatter: taskTranslateFormatter, sortable: true, minWidth: 100, filterable: true, params: { useFormatterOuputToFilter: true } };
+    const newCol = { id: `title${this.duplicateTitleHeaderCount++}`, field: 'id', nameKey: 'TITLE', formatter: taskFormatter, sortable: true, minWidth: 100, filterable: true, params: { useFormatterOuputToFilter: true } };
     this.columnDefinitions.push(newCol);
     this.columnDefinitions = this.columnDefinitions.slice(); // or use spread operator [...cols]
 
     // NOTE if you use an Extensions (Checkbox Selector, Row Detail, ...) that modifies the column definitions in any way
-    // you MUST use "getColumns()" instead to get ALL columns including the 1st column that is created internally
-    // for example if you use the Checkbox Selector (row selection), you need to use the code below
+    // you MUST use "getAllColumnDefinitions()" from the GridService, using this will be ALL columns including the 1st column that is created internally
+    // for example if you use the Checkbox Selector (row selection), you MUST use the code below
     /*
-    const allColumns = this.gridObj.getColumns();
+    const allColumns = this.aureliaGrid.gridService.getAllColumnDefinitions();
     allColumns.push(newCol);
     this.columnDefinitions = [...allColumns]; // (or use slice) reassign to column definitions for Aurelia to do dirty checking
     */
