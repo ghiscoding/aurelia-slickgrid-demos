69513a93acf5f1b1041302b4793f0e56dd244975
ghiscoding <gbeaulac@gmail.com>
2020-03-18 21:51:23 -0400

    update to latest version 2.17.9

--- webpack-bs4-demo/webpack.config.js
+++ webpack-bs4-demo/webpack.config.js
@@ -61,7 +61,7 @@ module.exports = ({ production } = {}, { extractCss, analyze, tests, hmr, port, 
       hidePathInfo: true, // prevents the path from being used in the filename when using maxSize
       chunks: "initial",
       // sizes are compared against source before minification
-      maxSize: 200000, // splits chunks if bigger than 200k, adjust as required (maxSize added in webpack v4.15)
+      maxSize: 500000, // splits chunks if bigger than 500k, adjust as required (maxSize added in webpack v4.15)
       cacheGroups: {
         default: false, // Disable the built-in groups default & vendors (vendors is redefined below)
         // You can insert additional cacheGroup entries here if you want to split out specific modules
@@ -94,7 +94,7 @@ module.exports = ({ production } = {}, { extractCss, analyze, tests, hmr, port, 
           chunks: 'async',
           priority: 9,
           reuseExistingChunk: true,
-          minSize: 10000  // use smaller minSize to avoid too much potential bundle bloat due to module duplication.
+          minSize: 20000  // use smaller minSize to avoid too much potential bundle bloat due to module duplication.
         },
         commonsAsync: { // commons async chunk, remaining asynchronously used modules as single chunk file
           name: 'commons.async',
@@ -102,7 +102,7 @@ module.exports = ({ production } = {}, { extractCss, analyze, tests, hmr, port, 
           chunks: 'async',
           priority: 0,
           reuseExistingChunk: true,
-          minSize: 10000  // use smaller minSize to avoid too much potential bundle bloat due to module duplication.
+          minSize: 20000  // use smaller minSize to avoid too much potential bundle bloat due to module duplication.
         }
       }
     }
@@ -114,7 +114,8 @@ module.exports = ({ production } = {}, { extractCss, analyze, tests, hmr, port, 
     historyApiFallback: true,
     hot: hmr || project.platform.hmr,
     port: port || project.platform.port,
-    host: host || project.platform.host
+    host: host || project.platform.host,
+    open: project.platform.open,
   },
   devtool: production ? 'nosources-source-map' : 'cheap-module-eval-source-map',
   module: {
