1b81ab1e57bff63466c48c0beb8d218c7805e9b2
ghiscoding <gbeaulac@gmail.com>
2020-06-30 00:05:38 -0400

    update github demo page to latest version 2.19.0

---
+++ docs/vendor~8365b14f.58e96e9068f0a14993a6.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],[,function(n,o,p){p("25Wt"),n.exports=p("+2Rf")}],[[1,4,0,1]]]);
+//# sourceMappingURL=vendor~8365b14f.58e96e9068f0a14993a6.bundle.map
