0b7123dad8742f87b1dc1eb287a0e45fb4b1679a
ghiscoding <gbeaulac@gmail.com>
2020-12-08 21:36:44 -0500

    refactor: fix i18n translations not working

--- webpack-bs4-demo/src/main.ts
+++ webpack-bs4-demo/src/main.ts
@@ -1,7 +1,7 @@
 import { Aurelia } from 'aurelia-framework'
 import { PLATFORM } from 'aurelia-pal';
 import { TCustomAttribute } from 'aurelia-i18n';
-import * as Backend from 'i18next-xhr-backend';
+import Backend from 'i18next-xhr-backend';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
@@ -10,7 +10,6 @@ import './styles/styles.scss';
 import './styles/slickgrid-styles.scss';
 import 'multiple-select-modified/src/multiple-select.css';
 import 'multiple-select-modified/src/multiple-select.js';
-// import environment from './environment';
 import * as environment from '../config/environment.json';
 import * as Bluebird from 'bluebird';
 import 'bootstrap';
@@ -36,22 +35,22 @@ export function configure(aurelia: Aurelia) {
     config.options.gridMenu.iconCssClass = 'fa fa-bars';
   });
 
+  // aurelia i18n to handle multiple locales
   aurelia.use.plugin(PLATFORM.moduleName('aurelia-i18n'), instance => {
-    let aliases = ['t', 'i18n'];
+    const aliases = ['t', 'i18n'];
     // add aliases for 't' attribute
     TCustomAttribute.configureAliases(aliases);
 
     // register backend plugin
     instance.i18next.use(Backend);
 
-    // adapt options to your needs (see http://i18next.com/docs/options/)
-    // make sure to return the promise of the setup method, in order to guarantee proper loading
     return instance.setup({
-      backend: {                                  // <-- configure backend settings
-        loadPath: 'assets/locales/{{lng}}/{{ns}}.json', // <-- XHR settings for where to get the files from
+      backend: {
+        loadPath: 'assets/locales/{{lng}}/{{ns}}.json',
       },
-      attributes: aliases,
       lng: 'en',
+      defaultNS: 'translation',
+      attributes: aliases,
       fallbackLng: 'en',
       debug: false
     });
