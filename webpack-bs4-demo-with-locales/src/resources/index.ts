8866f87a7746e5d4a36f19fb3b565191b337d2a2
ghiscoding <gbeaulac@gmail.com>
2021-10-25 22:44:43 -0400

    demo: add decimal value converter

--- webpack-bs4-demo-with-locales/src/resources/index.ts
+++ webpack-bs4-demo-with-locales/src/resources/index.ts
@@ -3,6 +3,7 @@ import { FrameworkConfiguration, PLATFORM } from 'aurelia-framework';
 export function configure(config: FrameworkConfiguration) {
   config.globalResources([
     PLATFORM.moduleName('./value-converters/date-format'),
+    PLATFORM.moduleName('./value-converters/decimal'),
     PLATFORM.moduleName('./value-converters/stringify')
   ]);
 }
