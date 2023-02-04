175a805be661cc72f69742a6fcd579bcea50e1db
ghiscoding <gbeaulac@gmail.com>
2023-02-04 15:52:44 -0500

    chore: use overflow on row selection when selecting all rows

--- webpack-bs4-demo/src/examples/slickgrid/example10.ts
+++ webpack-bs4-demo/src/examples/slickgrid/example10.ts
@@ -15,17 +15,17 @@ export class Example2 {
   `;
   @bindable() isGrid2WithPagination = true;
 
-  aureliaGrid1: AureliaGridInstance;
-  aureliaGrid2: AureliaGridInstance;
-  columnDefinitions1: Column[];
-  columnDefinitions2: Column[];
-  gridOptions1: GridOption;
-  gridOptions2: GridOption;
-  dataset1: any[];
-  dataset2: any[];
-  selectedTitles: any[];
+  aureliaGrid1!: AureliaGridInstance;
+  aureliaGrid2!: AureliaGridInstance;
+  columnDefinitions1: Column[] = [];
+  columnDefinitions2: Column[] = [];
+  gridOptions1!: GridOption;
+  gridOptions2!: GridOption;
+  dataset1: any[] = [];
+  dataset2: any[] = [];
+  selectedTitles = '';
   selectedTitle = '';
-  selectedGrid2IDs: number[];
+  selectedGrid2IDs: number[] = [];
 
   constructor() {
     // define the grid options & columns and then create the grid itself
@@ -104,15 +104,13 @@ export class Example2 {
       enableRowSelection: true,
       enableCheckboxSelector: true,
       enableFiltering: true,
-      gridHeight: 225,
-      gridWidth: 800,
       checkboxSelector: {
         // remove the unnecessary "Select All" checkbox in header when in single selection mode
         hideSelectAllCheckbox: true,
 
         // you can override the logic for showing (or not) the expand icon
         // for example, display the expand icon only on every 2nd row
-        // selectableOverride: (row: number, dataContext: any, grid: any) => (dataContext.id % 2 === 1)
+        // selectableOverride: (row: number, dataContext: any, grid: SlickGrid) => (dataContext.id % 2 === 1)
       },
       multiSelect: false,
       rowSelectionOptions: {
@@ -125,6 +123,8 @@ export class Example2 {
       gridMenu: {
         hideForceFitButton: true
       },
+      gridHeight: 225,
+      gridWidth: 800,
       enablePagination: true,
       pagination: {
         pageSizes: [5, 10, 15, 20, 25, 50, 75, 100],
@@ -140,12 +140,14 @@ export class Example2 {
       enableAutoResize: false,
       enableCellNavigation: true,
       enableFiltering: true,
-      gridHeight: 255,
-      gridWidth: 800,
       checkboxSelector: {
+        // optionally change the column index position of the icon (defaults to 0)
+        // columnIndexPosition: 1,
+
         // you can toggle these 2 properties to show the "select all" checkbox in different location
         hideInFilterHeaderRow: false,
-        hideInColumnTitleRow: true
+        hideInColumnTitleRow: true,
+        applySelectOnAllPages: true, // when clicking "Select All", should we apply it to all pages (defaults to true)
       },
       rowSelectionOptions: {
         // True (Single Selection), False (Multiple Selections)
@@ -153,6 +155,8 @@ export class Example2 {
       },
       enableCheckboxSelector: true,
       enableRowSelection: true,
+      gridHeight: 255,
+      gridWidth: 800,
       enablePagination: true,
       pagination: {
         pageSizes: [5, 10, 15, 20, 25, 50, 75, 100],
@@ -198,19 +202,19 @@ export class Example2 {
   }
 
   goToGrid1FirstPage() {
-    this.aureliaGrid1.paginationService.goToFirstPage();
+    this.aureliaGrid1.paginationService!.goToFirstPage();
   }
 
   goToGrid1LastPage() {
-    this.aureliaGrid1.paginationService.goToLastPage();
+    this.aureliaGrid1.paginationService!.goToLastPage();
   }
 
   goToGrid2FirstPage() {
-    this.aureliaGrid2.paginationService.goToFirstPage();
+    this.aureliaGrid2.paginationService!.goToFirstPage();
   }
 
   goToGrid2LastPage() {
-    this.aureliaGrid2.paginationService.goToLastPage();
+    this.aureliaGrid2.paginationService!.goToLastPage();
   }
 
   /** Dispatched event of a Grid State Changed event */
@@ -224,10 +228,13 @@ export class Example2 {
     console.log('Grid State changed:: ', gridStateChanges);
     console.log('Grid State changed:: ', gridStateChanges.change);
 
-    if (gridStateChanges.gridState.rowSelection) {
-      this.selectedGrid2IDs = (gridStateChanges.gridState.rowSelection.filteredDataContextIds || []) as number[];
+    if (gridStateChanges.gridState!.rowSelection) {
+      this.selectedGrid2IDs = (gridStateChanges.gridState!.rowSelection.filteredDataContextIds || []) as number[];
       this.selectedGrid2IDs = this.selectedGrid2IDs.sort((a, b) => a - b); // sort by ID
-      this.selectedTitles = this.selectedGrid2IDs.map(dataContextId => `Task ${dataContextId}`);
+      this.selectedTitles = this.selectedGrid2IDs.map(dataContextId => `Task ${dataContextId}`).join(',');
+      if (this.selectedTitles.length > 293) {
+        this.selectedTitles = this.selectedTitles.substring(0, 293) + '...';
+      }
     }
   }
 
@@ -235,14 +242,13 @@ export class Example2 {
   // IMPORTANT, the Pagination MUST BE CREATED on initial page load before you can start toggling it
   // Basically you cannot toggle a Pagination that doesn't exist (must created at the time as the grid)
   isGrid2WithPaginationChanged() {
-    // this.isGrid2WithPagination = !this.isGrid2WithPagination;
-    this.aureliaGrid2.paginationService.togglePaginationVisibility(this.isGrid2WithPagination);
+    this.aureliaGrid2.paginationService!.togglePaginationVisibility(this.isGrid2WithPagination);
   }
 
-  onGrid1SelectedRowsChanged(e, args) {
+  onGrid1SelectedRowsChanged(_e: Event, args: any) {
     const grid = args && args.grid;
     if (Array.isArray(args.rows)) {
-      this.selectedTitle = args.rows.map(idx => {
+      this.selectedTitle = args.rows.map((idx: number) => {
         const item = grid.getDataItem(idx);
         return item && item.title || '';
       });
