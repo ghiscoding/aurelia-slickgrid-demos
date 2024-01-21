070d0ff4d4ed85f583fd07027fe0a00604cd4a84
ghiscoding <gbeaulac@gmail.com>
2024-01-21 16:38:16 -0500

    chore: update to latest v7.3.0

--- webpack-bs5-demo/src/examples/slickgrid/example20.ts
+++ webpack-bs5-demo/src/examples/slickgrid/example20.ts
@@ -48,8 +48,8 @@ export class Example20 {
     // with frozen (pinned) grid, in order to see the entire row being highlighted when hovering
     // we need to do some extra tricks (that is because frozen grids use 2 separate div containers)
     // the trick is to use row selection to highlight when hovering current row and remove selection once we're not
-    this.slickEventHandler.subscribe(this.gridObj.onMouseEnter, (event: Event) => this.highlightRow(event, true));
-    this.slickEventHandler.subscribe(this.gridObj.onMouseLeave, (event: Event) => this.highlightRow(event, false));
+    this.slickEventHandler.subscribe(this.gridObj.onMouseEnter, (event: Event) => this.colorizeHoveringRow(event, true));
+    this.slickEventHandler.subscribe(this.gridObj.onMouseLeave, (event: Event) => this.colorizeHoveringRow(event, false));
   }
 
   highlightRow(event: Event, isMouseEnter: boolean) {
