bffef2440f018aa2f3f5bb95a1bea813dcd8ce70
ghiscoding <gbeaulac@gmail.com>
2024-03-31 23:12:53 -0400

    chore: update to latest v7.6.1

--- webpack-bs5-demo/src/examples/slickgrid/example30.ts
+++ webpack-bs5-demo/src/examples/slickgrid/example30.ts
@@ -1,6 +1,7 @@
 import { IHttpClient } from '@aurelia/fetch-client';
 import { newInstanceOf } from '@aurelia/kernel';
 import { ExcelExportService } from '@slickgrid-universal/excel-export';
+import { SlickCustomTooltip } from '@slickgrid-universal/custom-tooltip-plugin';
 import { SlickCompositeEditor, SlickCompositeEditorComponent } from '@slickgrid-universal/composite-editor-component';
 
 import {
@@ -126,7 +127,8 @@ export class Example30 {
   defineGrids() {
     this.columnDefinitions = [
       {
-        id: 'title', name: 'Title', field: 'title', sortable: true, type: FieldType.string, minWidth: 75,
+        id: 'title', name: '<span title="Task must always be followed by a number" class="color-warning-dark fa fa-exclamation-triangle"></span> Title <span title="Title is always rendered as UPPERCASE" class="fa fa-info-circle"></span>',
+        field: 'title', sortable: true, type: FieldType.string, minWidth: 75,
         cssClass: 'text-uppercase fw-bold', columnGroup: 'Common Factor',
         filterable: true, filter: { model: Filters.compoundInputText },
         editor: {
@@ -391,7 +393,7 @@ export class Example30 {
       excelExportOptions: {
         exportWithFormatter: false
       },
-      externalResources: [new ExcelExportService(), this.compositeEditorInstance],
+      externalResources: [new ExcelExportService(), new SlickCustomTooltip(), this.compositeEditorInstance],
       enableFiltering: true,
       rowSelectionOptions: {
         // True (Single Selection), False (Multiple Selections)
@@ -497,7 +499,7 @@ export class Example30 {
     this.aureliaGrid = aureliaGrid;
   }
 
-  handleValidationError(_e: Event, args: any) {
+  handleValidationError(e: Event, args: any) {
     if (args.validationResults) {
       let errorMsg = args.validationResults.msg || '';
       if (args.editor && (args.editor instanceof SlickCompositeEditor)) {
@@ -513,6 +515,7 @@ export class Example30 {
     } else {
       alert(args.validationResults.msg);
     }
+    e.preventDefault(); // OR eventData.preventDefault();
     return false;
   }
 
@@ -525,7 +528,7 @@ export class Example30 {
 
     if (column && item) {
       if (!checkItemIsEditable(item, column, grid)) {
-        e.stopImmediatePropagation();
+        e.preventDefault(); // OR eventData.preventDefault();
         return false;
       }
     }
