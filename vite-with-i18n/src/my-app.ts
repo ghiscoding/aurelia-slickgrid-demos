2f40596452cb1cdaf15ff8cede7412a7810d3dae
ghiscoding <gbeaulac@gmail.com>
2026-05-02 16:33:07 -0400

    chore: add prettier config

--- vite-with-i18n/src/my-app.ts
+++ vite-with-i18n/src/my-app.ts
@@ -1,10 +1,9 @@
-import { route, type Routeable } from "@aurelia/router";
-import { customElement } from "aurelia";
-import AureliaLogo from "./assets/aurelia-logo.png?url";
-import template from "./my-app.html?raw";
-import "./styles.scss";
+import { route, type Routeable } from '@aurelia/router';
+import { customElement } from 'aurelia';
+import AureliaLogo from './assets/aurelia-logo.png?url';
+import template from './my-app.html?raw';
+import './styles.scss';
 
-// prettier-ignore
 const myRoutes: Routeable[] = [
   { path: '', redirectTo: 'example01' },
   { path: 'example01', component: () => import('./examples/slickgrid/example01.js'), title: '1- Basic Grid / 2 Grids' },
@@ -64,9 +63,9 @@ const myRoutes: Routeable[] = [
 ];
 @route({
   routes: myRoutes,
-  fallback: "example01",
+  fallback: 'example01',
 })
-@customElement({ name: "my-app", template })
+@customElement({ name: 'my-app', template })
 export class MyApp {
   aureliaLogo = AureliaLogo;
   routes = myRoutes;
@@ -76,8 +75,8 @@ export class MyApp {
 
     // scroll to active link route, there's probably a better way to do this but couldn't find lifecycle for it
     setTimeout(() => {
-      const linkElm = document.querySelector(".nav-link.active");
-      linkElm?.scrollIntoView({ block: "nearest" });
+      const linkElm = document.querySelector('.nav-link.active');
+      linkElm?.scrollIntoView({ block: 'nearest' });
     }, 45);
   }
 
@@ -85,19 +84,15 @@ export class MyApp {
     // GitHub logo with Stars shouldn't be created while testing in Cypress (which always wait few seconds even minutes to load the logo)
     // <a href="https://github.com/ghiscoding/slickgrid-universal"><img src="https://img.shields.io/github/stars/ghiscoding/slickgrid-universal?style=social"></a>
     const decodedCookie = decodeURIComponent(document.cookie);
-    if (decodedCookie !== "serve-mode=cypress") {
-      const ghStarLinkElm = document.createElement("a");
-      ghStarLinkElm.href =
-        "https://github.com/ghiscoding/slickgrid-universal/tree/master/frameworks/aurelia-slickgrid";
+    if (decodedCookie !== 'serve-mode=cypress') {
+      const ghStarLinkElm = document.createElement('a');
+      ghStarLinkElm.href = 'https://github.com/ghiscoding/slickgrid-universal/tree/master/frameworks/aurelia-slickgrid';
 
-      const imgStarElm = document.createElement("img");
-      imgStarElm.src =
-        "https://img.shields.io/github/stars/ghiscoding/slickgrid-universal?style=social";
+      const imgStarElm = document.createElement('img');
+      imgStarElm.src = 'https://img.shields.io/github/stars/ghiscoding/slickgrid-universal?style=social';
 
-      const ghButtonContainerElm = document.querySelector(
-        ".github-button-container",
-      );
-      if (ghButtonContainerElm && !ghButtonContainerElm.querySelector("a")) {
+      const ghButtonContainerElm = document.querySelector('.github-button-container');
+      if (ghButtonContainerElm && !ghButtonContainerElm.querySelector('a')) {
         ghStarLinkElm.appendChild(imgStarElm);
         ghButtonContainerElm.appendChild(ghStarLinkElm);
       }
